9520cbf41095602b6e54ca5adfc9a3520f49da71
Raja Awais Khalil <awaiskhalil.pk@gmail.com>
2018-09-19 23:17:48 +0500

    post-method in php

---
+++ post-method.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["f_name"]. $_POST["l_name"]; ?><br>
+You are successfully registered with <?php echo $_POST["email"]; ?> <br>
+Your Mobile no is : <?php echo $_POST["digits"] ?>
+
+<p style="color:blue" style="font-family:courier new"> Thankyou for registering with us! </p>
+</body>
+</html>
